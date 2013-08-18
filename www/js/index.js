a4f9cfe1877ca54c5ca5e31201443dfce4d38817
Nick Breen <nick@nickbreen.ca>
2013-08-17 22:14:56 -0400

    Installed connection plugin and gps plugin

--- www/js/index.js
+++ www/js/index.js
@@ -33,7 +33,8 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        alert('onDeviceReady has been recieved.');
+	app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
