de166d9eb3a30ae5066dc25f30641585c137cc72
davidrs <david.rs@hotmail.com>
2013-09-05 17:48:36 -0400

    Added constants to config.php

--- server/config.php
+++ server/config.php
@@ -1,11 +1,17 @@
 <?php 
 header('Access-Control-Allow-Origin: *');
 
+define('NUM_USERS', '110');			// # of users to be added to DB
+define('PASSCODE_RANGE', '100');	// unique passcodes will use a random number in this range 
+define('PAGE_SECRET', 'Britta');	// secret that map.js will post with getMarker calls
 
+// Login Database details
 $dbName='your_db';
 $dbUser='your_db';
 $dbPwd='your_pwd';
 
+
+// Setup Database Connection
 $con = mysql_connect("localhost",$dbUser,$dbPwd);
 if (!$con)
 {
@@ -15,7 +21,7 @@ if (!$con)
 // Set the active mySQL database
 $db_selected =mysql_select_db($dbName, $con);
 if (!$db_selected) {
-  die ("Can\'t use db : " . mysql_error());
+	die ("Can\'t use db : " . mysql_error());
 }
 
 ?>
